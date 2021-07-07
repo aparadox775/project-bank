b47dfc0686b5cd86bf82bd76f1feb408fed2e066
aparadox775 <amirhosin775@gmail.com>
2021-07-07 20:18:48 +0430

    //////

---
+++ include/bank.hpp
@@ -0,0 +1,15 @@
+//
+// Created by aparadox775 on 7/7/21.
+//
+
+#ifndef GOLS_BANK_HPP
+#define GOLS_BANK_HPP
+
+
+class bank
+{
+
+};
+
+
+#endif //GOLS_BANK_HPP
---
+++ src/bank.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aparadox775 on 7/7/21.
+//
+
+#include "bank.hpp"
